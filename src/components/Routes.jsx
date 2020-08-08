3dd719644352b670c3cad5092e2a235c9d047d27
Dong-Young Kim <31337.persona@gmail.com>
2020-08-09 06:07:46 +0900

    fix(routes): fix router

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import SignIn from "./Auth/signin";
 import SignUp from "./Auth/signup";
-import Map from "./Map";
 import Write from "./Edit/write";
+import Map from "./Map";
 
 const Routes = () => (
   <Router>
-    <Route path="/" component={Map} />
+    <Route path="/map" component={Map} />
     <Route path="/write" component={Write} />
     <Route path="/auth/signup" component={SignUp} />
     <Route path="/auth/signin" component={SignIn} />
